9b52473a7da50f837eaa167503596212d0fc34a5
harimancodes <sreehariperumbavoor@gmail.com>
2021-05-26 19:25:00 +0530

    commit

---
+++ Java/InvertedFullPyramid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class InvertedFullPyramid {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
+        int n=sc.nextInt();
+
+        for(int i=n;i>0;i--){
+            for(int j=n-i;j>0;j--){
+                System.out.print(" ");
+            }
+            for(int k=0;k<i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
